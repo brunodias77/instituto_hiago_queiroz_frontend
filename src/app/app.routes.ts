25982d71a348f39137dc3681170d1be03ba3746e
brunodias77 <brunohdias95@gmail.com>
2024-12-01 20:25:13 -0300

    Adiciona componente Footer e Post, atualiza rotas e inclui Footer no layout principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'post', component: PostComponent },
+];
