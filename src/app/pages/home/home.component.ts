d9ece77aa090871d5841efafb892be805ef692da
brunodias77 <brunohdias95@gmail.com>
2024-12-15 23:10:38 -0300

    Adiciona interface Card e implementa lista de cards no componente Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CountUpModule } from 'ngx-countup';
+import { Card } from '../../types';
+import { ButtonComponent } from '../../components/shared/button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CountUpModule],
+  imports: [CountUpModule, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -16,6 +18,32 @@ export class HomeComponent {
   collectedFood: number = 0;
   socialActions: number = 0;
   plasticLid: number = 0;
+  cards: Card[] = [
+    {
+      imageSrc: 'https://pagedone.io/asset/uploads/1696244553.png',
+      imageAlt: 'Harsh image',
+      title: 'Fintech 101: Exploring the Basics of Electronic Payments',
+      author: 'Harsh C.',
+      timeAgo: '2 year ago',
+    },
+    {
+      imageSrc: 'https://pagedone.io/asset/uploads/1696244579.png',
+      imageAlt: 'John image',
+      title:
+        'From Classroom to Cyberspace: The Growing Influence of EdTech in Fintech',
+      author: 'John D.',
+      timeAgo: '2 year ago',
+    },
+    {
+      imageSrc: 'https://pagedone.io/asset/uploads/1696244619.png',
+      imageAlt: 'Alexa image',
+      title:
+        'Fintech Solutions for Student Loans: Easing the Burden of Education Debt',
+      author: 'Alexa H.',
+      timeAgo: '2 year ago',
+    },
+  ];
+
   constructor() {
     this.startAnimation();
   }
---
+++ src/app/types.ts
@@ -0,0 +1,7 @@
+export interface Card {
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  author: string;
+  timeAgo: string;
+}
